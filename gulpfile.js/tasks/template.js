a94e4296641c03b9ccd677a5af029b0cc3fbb994
Sergey Glazov <yaglazov@gmail.com>
2019-05-25 17:44:10 +0300

    🐣

--- gulpfile.js/tasks/template.js
+++ gulpfile.js/tasks/template.js
@@ -4,22 +4,20 @@ const nunjucksRender = require('gulp-nunjucks-render');
 const markdown       = require('nunjucks-markdown');
 const marked         = require('marked');
 const inlineCss      = require('gulp-inline-css');
-const notify         = require("gulp-notify");
 
 const config         = require('../config');
 
-let nunjucksMarkdown = function(env) {
+const nunjucksMarkdown = function(env) {
   markdown.register(env, marked);
 };
 
 
 // Шаблонизация
 gulp.task('template', function() {
-
   return gulp.src(config.source.templates)
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(nunjucksRender({
-      path: 'src/templates',
+      path:     'src/templates',
       manageEnv: nunjucksMarkdown
     }))
     .pipe(inlineCss({
@@ -28,9 +26,6 @@ gulp.task('template', function() {
       removeLinkTags: true,
       applyLinkTags: true,
       preserveMediaQueries: true
-    }))
-    .pipe(notify({
-      message: 'Pechkin: template task'
     }))
     .pipe(gulp.dest(config.build.html));
 });
